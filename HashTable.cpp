7bb17ebe07c0e383cdb1559fc0a4c043610cccb8
Vishvak03 <vishvak.logar_asp25@ashoka.edu.in>
2024-10-19 17:16:08 +0530

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -147,30 +147,3 @@ public:
         cout << endl;
     }
 };
-
-// Sample main.cpp for testing
-int main() {
-    int initialSize = 5; 
-    HashTable ht(initialSize);
-
-    // Example test case
-    ht.insert(1);
-    ht.printTable();
-    ht.insert(6);
-    ht.printTable();
-    ht.insert(15);
-    ht.printTable(); 
-    ht.insert(25);
-    ht.printTable();
-    ht.remove(15);
-    ht.printTable();
-    ht.insert(29);  
-    ht.printTable(); 
-    ht.insert(33);  
-    ht.printTable(); 
-
-    int find = ht.search(22);
-    std::cout << "Found at:" << find << std::endl;
-
-    return 0;
-}
